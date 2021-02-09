22a9f217478b763f031213d82fc2f2f26387874d
JustinWomack <womack.justin@rocketmail.com>
2021-02-08 18:37:35 -0600

    add https

--- index.ts
+++ index.ts
@@ -1,32 +1,36 @@
 import express, { Request, Response, NextFunction, Errback } from 'express';
-import dotenv from 'dotenv';
 import http from 'http';
 import https from 'https';
 import next from 'next';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
 nextApp.prepare()
     .then(() => {
-        const server = express();
+        const app = express();
+        const server = process.env.PROTO === 'http' ? new http.Server(app) : https.createServer({
+            key: fs.readFileSync(`${process.cwd()}/server.key`),
+            cert: fs.readFileSync(`${process.cwd()}/server.crt`),
+        }, app);
 
-        server.use(bodyParser.json());
-        server.all('*', (req: Request, res: Response) => {
+        app.use(bodyParser.json());
+        app.all('*', (req: Request, res: Response) => {
             return handle(req, res);
         });
 
-        server.use('*', (err: Errback, req: Request, res: Response, n: NextFunction) => {
+        app.use('*', (err: Errback, req: Request, res: Response, n: NextFunction) => {
             console.log(err);
         });
 
         server.listen(port, () => {
-            console.log(`> Ready on http://localhost:${port}`)
+            console.log(`> Ready on ${port}`)
         })
     });
